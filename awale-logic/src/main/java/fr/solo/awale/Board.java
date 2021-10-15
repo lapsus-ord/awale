382d1d5ad9113d73ec03f5a79680906e44f6e096
Rotashin <mateo.fernandez73@gmail.com>
2021-10-16 01:02:31 +0200

    :memo: Ajout de javadoc pour les méthodes de la classe + Traduction des termes français en anglais

--- awale-logic/src/main/java/fr/solo/awale/Board.java
+++ awale-logic/src/main/java/fr/solo/awale/Board.java
@@ -4,19 +4,20 @@ import java.util.Arrays;
 
 public class Board {
     private int[][] cells;
-    private int nbGraines;
+    private int nbOfSeeds;
 
     public Board() {
         cells = new int[2][6];
-        nbGraines = 48;
+        nbOfSeeds = 48;
         initBoard();
     }
 
     public Board(int[][] cells) {
         this.cells = cells;
-        nbGraines = 48;
+        nbOfSeeds = 48;
     }
 
+    // Remplit le plateau de graines
     private void initBoard() {
         Arrays.fill(cells[0], 4);
         Arrays.fill(cells[1], 4);
@@ -26,17 +27,25 @@ public class Board {
      * @param side {@code TOP} = 1ère ligne ; {@code BOTTOM} = 2e ligne
      * @return Un tableau contenant les trous du côté de {@code nb}
      */
-    public int[] getLine(Side side) {
+    public int[] getRow(Side side) {
         if (side.equals(Side.TOP))
             return cells[0];
         return cells[1];
     }
 
-    public boolean isPlayable(int trou, Side side) {
-        if (getLine(side)[trou] == 0)
+    /**
+     * Indique si un trou peut être joué.
+     *
+     * @param hole Le trou choisi.
+     * @param side Le côté du plateau choisi.
+     * @return {@code true}/{@code false} = Selon si le trou peut être joué.
+     */
+    public boolean isPlayable(int hole, Side side) {
+        if (getRow(side)[hole] == 0)
             return false;
-        if (getGraineInLine(getOppositeSide(side)) == 0) { // Cas Affamé
-            return (getLine(side)[trou] >= (cells[0].length - trou));
+        if (getSeedInRow(getOppositeSide(side)) == 0) { // Cas Affamé :
+            // Renvoie true si on peut utiliser ce trou pour distribuer chez l'ennemi
+            return (getRow(side)[hole] >= (cells[0].length - hole));
         }
         return true;
     }
@@ -49,12 +58,12 @@ public class Board {
      * Vide le trou passé en paramètre.
      *
      * @param side Le côté du plateau (ou la ligne), i∈[0, 1]
-     * @param trou Le numéro du trou (ou la colonne), j∈[0, 5]
+     * @param hole Le numéro du trou (ou la colonne), j∈[0, 5]
      * @return Le nombre de graines dans le trou
      */
-    public int clearTrou(Side side, int trou) {
-        int stock = getLine(side)[trou];
-        getLine(side)[trou] = 0;
+    public int clearHole(Side side, int hole) {
+        int stock = getRow(side)[hole];
+        getRow(side)[hole] = 0;
         return stock;
     }
 
@@ -62,34 +71,39 @@ public class Board {
      * Ajoute une graine dans un trou.
      *
      * @param side Le côté du plateau (ou la ligne), i∈[0, 1]
-     * @param trou Le numéro du trou (ou la colonne), j∈[0, 5]
+     * @param hole Le numéro du trou (ou la colonne), j∈[0, 5]
      * @see Player#play(int)
      */
-    public void addGraine(Side side, int trou) {
-        getLine(side)[trou]++;
+    public void addSeed(Side side, int hole) {
+        getRow(side)[hole]++;
     }
 
     /**
      * Ramasser les graines dans un trou (comprend la rafle).
      *
      * @param side Le côté du plateau (ou la ligne), i∈[0, 1]
-     * @param trou Le numéro du trou (ou la colonne), j∈[0, 5]
+     * @param hole Le numéro du trou (ou la colonne), j∈[0, 5]
      * @return Le nombre de graine récupéré par le ramassage.
+     * @see Player#captureSeed(Board, int)
      */
-    public int ramasser(Side side, int trou) {
-        if (getLine(side)[trou] == 2 || getLine(side)[trou] == 3) {
-            if (trou == 0) {
-                return clearTrou(side, trou);
+    public int capturing(Side side, int hole) {
+        if (getRow(side)[hole] == 2 || getRow(side)[hole] == 3) {
+            if (hole == 0) {
+                return clearHole(side, hole);
             } else {
-                return clearTrou(side, trou) + ramasser(side, trou - 1);
+                return clearHole(side, hole) + capturing(side, hole - 1);
             }
         } else {
             return 0;
         }
     }
 
-    public int getGraineInLine(Side s) {
-        int[] t = getLine(s);
+    /**
+     * @param s Le côté du plateau.
+     * @return Renvoie le nombre de graines dans une ligne.
+     */
+    public int getSeedInRow(Side s) {
+        int[] t = getRow(s);
         int n = 0;
         for (int j : t) {
             if (j != 0) {
@@ -99,7 +113,12 @@ public class Board {
         return n;
     }
 
-    public void removeNbGraine(int ramassage) {
-        this.nbGraines -= ramassage;
+    /**
+     * Enlève un nombre de graines du nombre de graines sur le plateau.
+     *
+     * @param nb Le nombre de graines capturées.
+     */
+    public void removeNbSeed(int nb) {
+        this.nbOfSeeds -= nb;
     }
 }
