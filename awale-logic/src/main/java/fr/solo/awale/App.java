f4e17fb749595084c3d1958bb933a6206e06aeb8
Rotashin <mateo.fernandez@etu.umontpellier.fr>
2021-11-07 23:54:48 +0100

    :sparkles: Une IA débile ! wééééé

--- awale-logic/src/main/java/fr/solo/awale/App.java
+++ awale-logic/src/main/java/fr/solo/awale/App.java
@@ -1,5 +1,6 @@
 package fr.solo.awale;
 
+import fr.solo.awale.player.ai.DumbAI;
 import fr.solo.awale.player.ai.SmartAI;
 import fr.solo.awale.player.AbstractPlayer;
 import fr.solo.awale.player.Player;
@@ -9,7 +10,7 @@ import static com.diogonunes.jcolor.Attribute.TEXT_COLOR;
 public class App {
     public static void main(String[] args) {
         AbstractPlayer p1 = new Player("Bernard", TEXT_COLOR(42, 157, 143));
-        AbstractPlayer p2 = new SmartAI("Skynet", 6);
+        AbstractPlayer p2 = new DumbAI("Chaosnet");
 
         Awale jeu = new Awale();
         p1.joinGame(jeu);
