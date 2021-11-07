6c65a032a3036acbddb0808bf977d832d9d1dc1a
Rotashin <mateo.fernandez@etu.umontpellier.fr>
2021-11-07 02:11:21 +0100

    Maintenant on peut avoir un arbre un peu plus grand

--- awale-logic/src/main/java/fr/solo/awale/App.java
+++ awale-logic/src/main/java/fr/solo/awale/App.java
@@ -7,7 +7,7 @@ import static com.diogonunes.jcolor.Attribute.TEXT_COLOR;
 public class App {
     public static void main(String[] args) {
         Player p1 = new Player("Bernard", TEXT_COLOR(42, 157, 143));
-        Player p2 = new SmartAI("Skynet", 4);
+        Player p2 = new SmartAI("Skynet", 6);
 
         Awale jeu = new Awale();
         p1.joinGame(jeu);
