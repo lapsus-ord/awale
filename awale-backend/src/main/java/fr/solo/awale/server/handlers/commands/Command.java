89afb099b7be91a45a07d264d5a2f127cdb16688
Fernandez Mateo <mateo.fernandez@etu.umontpellier.fr>
2021-12-04 17:43:19 +0000

    pourquoi y avait un historique ici ??

--- awale-backend/src/main/java/fr/solo/awale/server/handlers/commands/Command.java
+++ awale-backend/src/main/java/fr/solo/awale/server/handlers/commands/Command.java
@@ -7,12 +7,10 @@ import org.springframework.boot.json.JsonParserFactory;
 public abstract class Command {
     protected GameService gameService;
     protected JsonParser jsonParser;
-    private CommandHistory history;
 
     public Command() {
         this.gameService = GameService.getInstance();
         jsonParser = JsonParserFactory.getJsonParser();
-        history = new CommandHistory();
     }
 
     public abstract boolean execute(String payload);
