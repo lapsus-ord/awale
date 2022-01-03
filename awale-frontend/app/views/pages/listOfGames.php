8a98b0ece52a8db6f90bc38ac0a3e30acb789f11
Fernandez Mateo <mateo.fernandez@etu.umontpellier.fr>
2022-01-03 10:50:16 +0100

    Changement titre site Awale -> AWALEAU \o/

--- awale-frontend/app/views/pages/listOfGames.php
+++ awale-frontend/app/views/pages/listOfGames.php
@@ -1,6 +1,6 @@
 <div class="title">
   <img class="logo" src="assets/img/logo.png" alt="Logo du site Awale"/>
-  <h1>AWALE</h1>
+  <h1 style="color:#9fe592;">AWAL<span style="color:#3c8db9;">EAU</span></h1>
 </div>
 <div class="main">
   <div class="title" style="display: flex; align-items: center; gap: 100px">
